a4c3689f34d67249811ff31f732a7e3e2caedf57
maryannemm <maryannemaree@gmail.com>
2023-08-10 01:26:19 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,11 +8,9 @@
  */
 int main(void)
 {
-    /* Use write to print to standard error */
-    write(STDERR_FILENO,
-          "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-          strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	write(STDERR_FILENO,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 
-    /* Return 1 to indicate error */
-    return (1);
+	return (1);
 }

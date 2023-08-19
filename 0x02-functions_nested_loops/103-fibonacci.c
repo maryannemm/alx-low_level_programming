9fd67bdc76f8a83ef4ac9af6aa541a0acff94109
maryannemm <maryannemaree@gmail.com>
2023-08-19 10:54:35 -0700

    finnoccabi

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 
-/**
- * main - Finds and prints the sum of even-valued Fibonacci terms
- *        that do not exceed 4,000,000.
- *
- * Return: Always 0.
- */
-
 int main(void)
 {
-	long int fib1 = 1, fib2 = 2, nextFib;
-	long int sum = 2; // Start with 2 since the second term is even
+    int term1 = 1;  /* First term */
+    int term2 = 2;  /* Second term */
+    int nextTerm = 0;  /* To store the next term */
+    int sum = 2;  /* Sum of even-valued terms, initialized with the second term */
 
-	while (1)
-	{
-		nextFib = fib1 + fib2;
-		if (nextFib > 4000000)
-			break;
+    while (nextTerm <= 4000000)
+    {
+        nextTerm = term1 + term2;
 
-		if (nextFib % 2 == 0)
-			sum += nextFib;
+        /* Check if the next term is even */
+        if (nextTerm % 2 == 0)
+        {
+            sum += nextTerm;
+        }
 
-		fib1 = fib2;
-		fib2 = nextFib;
-	}
+        term1 = term2;
+        term2 = nextTerm;
+    }
 
-	printf("%ld\n", sum);
+    printf("Sum of even-valued terms: %d\n", sum);
 
-	return (0);
+    return 0;
 }
